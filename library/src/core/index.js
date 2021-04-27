db3d6f9f6d27ede51bedaf723fbc7b3f2dbfcad9
Emil Sroka <srokaemil@gmail.com>
2021-04-27 19:57:48 +0200

    feat: add connection path algorithm

--- library/src/core/index.js
+++ library/src/core/index.js
@@ -17,5 +17,78 @@ window.CoNDeT.core = {
         hex += ("00" + b.toString(16)).substr(-2, 2).toUpperCase();
 
         return hex;
+    },
+  getLinePoints: function(props) {
+    var starPoint = props.startPoint;
+    var endPoint = props.endPoint;
+    var checkIfCanPlacePoint = props.freePlaceCallback;
+
+    var deltaX = Math.abs(startPoint.x - endPoint.x);
+    var deltaY = Math.abs(startPoint.y - endPoint.y);
+    while (deltaX > 10 && deltaY > 10) {
+      var path = constructPathFromLinesOfLength(deltaX, deltaY, starPoint, endPoint, 40);
+
+      if (path != null) {
+        var points = [];
+        while (path.prev != null) {
+          points.unshift({ x: path.x, y: path.y })
+          path = path.prev;
+        }
+        return points;
+      }
+
+      deltaX /= 2;
+      deltaY /= 2;
     }
+
+    return [starPoint, endPoint];
+
+    function constructPathFromLinesOfLength(verticalLength, horizontalLength, startPoint, endPoint, numberOfLinesLimiter) {
+      // BFS algorithm
+      var edges = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+      var queue = [];
+      var layer = 0;
+
+      queue.push(startPoint);
+
+      while(queue.length !== 0 && layer <= numberOfLinesLimiter){
+        layer += 1;
+        var size = queue.length;
+
+        for (var i=0; i<size; i++){
+          var currentPoint = queue.shift();
+
+          if (currentPoint.x === endPoint.x && currentPoint.y === endPoint.y) {
+            return currentPoint;
+          }
+
+          for (var j=0; j<edges.length; j++) {
+            var edge = edges[j];
+            if (checkIfCanPlaceLine(currentPoint, {x: currentPoint.x + horizontalLength * edge[0], y: currentPoint.y + verticalLength * edge[1]})) {
+              queue.push({x: currentPoint.x + horizontalLength * edge[0], y: currentPoint.y + verticalLength * edge[1], prev: currentPoint});
+            }
+          }
+        }
+      }
+
+      return null;
+    }
+
+    function checkIfCanPlaceLine(startPoint, endPoint) {
+      var deltaX = startPoint.x - endPoint.x;
+      var deltaY = startPoint.y - endPoint.y;
+
+      var numberOfChecks = Math.round(Math.max(Math.abs(deltaX), Math.abs(deltaY)) / 10) || 1;
+
+      for (var i=0; i<numberOfChecks; i++) {
+        if (!checkIfCanPlacePoint({
+          x: startPoint.x + deltaX/numberOfChecks * i,
+          y: startPoint.y + deltaY/numberOfChecks * i,
+        })) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
 }
