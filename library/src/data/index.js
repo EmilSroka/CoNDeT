d65f4bd1933ce1d4fb5ee08dff973ffe3543ca80
Hub <hubmiz20@gmail.com>
2021-05-02 18:55:13 +0200

    fix: state object change

--- library/src/data/index.js
+++ library/src/data/index.js
@@ -1,23 +1,24 @@
 window.CoNDeT.data = {
+  State: (function () {
+    function constructor() {}
 
+    constructor.prototype.state = [];
+    constructor.prototype.subscribers = [];
 
-function State(){
-
-    this.state = [];
-    this.subscribers = [];
-}
-State.prototype = {
-    subscribe: function(fun){
+    constructor.prototype.methods = {
+      subscribe: function (fun) {
         this.subscribers.push(fun);
-    },
-    set: function(state){
+      },
+      set: function (state) {
         this.state = state;
         this.call_subscribers();
-    },
-    call_subscribers: function(){
-        this.subscribers.forEach(element => {
-            element.call();
+      },
+      call_subscribers: function () {
+        this.subscribers.forEach((element) => {
+          element.call();
         });
-    }
-}
-}
+      },
+    };
+    return constructor;
+  })(),
+};
