b702b778cb13d819299fdd34aa6aae648253d800
Hub <hubmiz20@gmail.com>
2021-05-02 17:29:35 +0200

    feat: add State object

--- library/src/data/index.js
+++ library/src/data/index.js
@@ -1,1 +1,23 @@
-window.CoNDeT.data = {}
+window.CoNDeT.data = {
+
+
+function State(){
+
+    this.state = [];
+    this.subscribers = [];
+}
+State.prototype = {
+    subscribe: function(fun){
+        this.subscribers.push(fun);
+    },
+    set: function(state){
+        this.state = state;
+        this.call_subscribers();
+    },
+    call_subscribers: function(){
+        this.subscribers.forEach(element => {
+            element.call();
+        });
+    }
+}
+}
