7a4f5e4c1b67ecadf625e4d3eda203aae2f92383
M1LO234 <48175250+M1LO234@users.noreply.github.com>
2021-05-02 13:40:31 +0200

    feat: add Table component and subcomponents

--- library/src/ui/index.js
+++ library/src/ui/index.js
@@ -302,4 +302,161 @@ window.CoNDeT.ui = {
       return unused;
     }
   })(),
+  Table: (function () {
+    function constructor() {}
+
+    constructor.prototype = Object.create(window.CoNDeT.ui.BaseComponent);
+
+    constructor.prototype.onInit = function (common, props) {
+      this.ref = document.createElement("table");
+      this.ref.classList.add("condet-table");
+      common.deltaXY = this.getPosition();
+      this.ref.style.cssText = `position: absolute; left: ${common.deltaXY.x}; top: ${common.deltaXY.y}`;
+
+      this.children = [];
+      this.createChild(Name, { text: props.name });
+      this.createChild(Head, {
+        conditions: props.conditions,
+        decisions: props.decisions,
+      });
+      this.createChild(Body, { content: props.rows });
+    };
+    constructor.prototype.onUpdate = function (common, props) {
+      // mozna zalozyc Table bedzie mial zawsze trojke dzieci zawsze w tej samej kolejnosci?
+      this.children[0].onUpdate(common, { text: props.name });
+      this.children[1].onUpdate(common, {
+        conditions: props.conditions,
+        decisions: props.decisions,
+      });
+      this.children[2].onUpdate(common, { content: props.rows });
+      var currentPosition = this.getPosition();
+      this.ref.style.cssText = `position: absolute; left: ${currentPosition.x}; top: ${currentPosition.y}`;
+      common.deltaXY = currentPosition;
+    };
+    constructor.prototype.onDestroy = function (common) {
+      this.ref.remove();
+    };
+
+    constructor.prototype.getRowXY = function (rowNumber) {
+      return this.children[2].children[rowNumber].getConnectionXY();
+    };
+
+    return constructor;
+  })(),
+
+  Name: (function () {
+    function constructor() {}
+
+    constructor.prototype.onInit = function (common, props) {
+      this.ref = document.createElement("caption");
+      this.ref.innerHTML = props.text;
+      this.ref.classList.add("condet-caption");
+    };
+    constructor.prototype.onUpdate = function (common, props) {
+      this.ref.innerHTML = props.text;
+    };
+    constructor.prototype.onDestroy = function (common) {
+      this.ref.remove();
+    };
+
+    return constructor;
+  })(),
+
+  Head: (function () {
+    function constructor() {}
+
+    constructor.prototype.appendHeaders = function (headerRef, content) {
+      for (let i = 0; i < content.conditions.length; i++) {
+        let headerCol = document.createElement("th");
+        headerCol.classList.add("condition");
+        headerCol.innerHTML = content.conditions[i];
+        headerRef.appendChild(headerCol);
+      }
+
+      for (let i = 0; i < content.decisions.length; i++) {
+        let headerCol = document.createElement("th");
+        headerCol.classList.add("decision");
+        headerCol.innerHTML = content.decisions[i];
+        headerRef.appendChild(headerCol);
+      }
+    };
+
+    constructor.prototype.onInit = function (common, props) {
+      this.ref = document.createElement("thead");
+      this.headerRow = document.createElement("tr");
+      this.ref.appendChild(this.headerRow);
+
+      this.appendHeaders(this.headerRow, {
+        conditions: props.conditions,
+        decisions: props.decisions,
+      });
+    };
+    constructor.prototype.onUpdate = function (common, props) {
+      for (let i = 0; i < this.ref.children.length; i++) {
+        this.ref.removeChild(this.ref.children[i]);
+      }
+      this.appendHeaders(this.headerRow, {
+        conditions: props.conditions,
+        decisions: props.decisions,
+      });
+    };
+    constructor.prototype.onDestroy = function (common) {
+      this.ref.remove();
+    };
+
+    return constructor;
+  })(),
+
+  Body: (function () {
+    function constructor() {}
+
+    constructor.prototype.onInit = function (common, props) {
+      this.ref = document.createElement("tbody");
+      this.children = [];
+
+      for (let i = 0; i < props.content.length; i++) {
+        this.createChild(Row, { constent: props.content[i] });
+      }
+    };
+    constructor.prototype.onUpdate = function (common, props) {
+      for (let i = 0; i < this.children.length; i++) {
+        this.children[i].onDestroy(common);
+      }
+      for (let i = 0; i < props.content.length; i++) {
+        this.createChild(Row, { constent: props.content[i] });
+      }
+    };
+    constructor.prototype.onDestroy = function (common) {
+      this.ref.remove();
+    };
+
+    return constructor;
+  })(),
+
+  Row: (function () {
+    function constructor() {}
+
+    constructor.prototype.onInit = function (common, props) {
+      this.ref = document.createElement("tr");
+
+      for (let i = 0; i < props.content.length; i++) {
+        let cell = document.createElement("td");
+        cell.innerHTML = props.content[i];
+        this.ref.appendChild(cell);
+      }
+    };
+
+    constructor.prototype.onUpdate = function (common, props) {};
+    constructor.prototype.onDestroy = function (common) {
+      this.ref.remove();
+    };
+
+    constructor.prototype.getConnectionXY = function () {
+      var { rowX, rowY } = this.getPosition();
+      var { width, height } = this.getDimensions();
+      return { x: rowX + width, y: rowY + height / 2 };
+    };
+
+    return constructor;
+  })(),
 };
