ade85ae33d35fb5eff539bb63fa385d9551fbf0c
Hub <hubmiz20@gmail.com>
2021-05-03 20:46:48 +0200

    fix: not overwrite delete consollog add comment

--- library/src/data/index.js
+++ library/src/data/index.js
@@ -1,38 +1,60 @@
 window.CoNDeT.data = {
-  FileReaderWriter:{
-  
+  State: (function () {
+    function constructor() {
+      this.state = [];
+      this.subscribers = [];
+    }
 
-  readFromFile =function (cb) {
-    var element = document.createElement('input');
-    element.setAttribute('type','file');
-    element.setAttribute('accept','.json');
-    element.addEventListener('change',function(event) {
-     var file = event.target.files[0];
-     console.log(file);
-     var fileReader = new FileReader();
+    constructor.prototype.subscribe = function (subscriber) {
+      this.subscribers.push(subscriber);
+      subscriber(this.state);
+    };
+    constructor.prototype.setState = function (state) {
+      this.state = state;
+      this.callSubscribers();
+    };
+    constructor.prototype.callSubscribers = function () {
+      var self = this;
+      this.subscribers.forEach(function (cb) {
+        cb(self.state);
+      });
+    };
+    return constructor;
+  })(),
 
-     fileReader.onload = function(e){
-       cb(JSON.parse(e.target.result));
-       element.remove();
-     };
-     fileReader.readAsText(file);
-    });
-    element.click();
+  FileReaderWriter: {
+    readFromFile: function (cb) {
+      var element = document.createElement('input');
+      element.setAttribute('type', 'file');
+      element.setAttribute('accept', '.json');
+      element.addEventListener('change', function (event) {
+        var file = event.target.files[0];
+        var fileReader = new FileReader();
+
+        fileReader.onload = function (e) {
+          cb(JSON.parse(e.target.result));
+          element.remove();
+        };
+        fileReader.readAsText(file);
+      });
+      element.click();
     },
-    saveToFile = function (FileName, Data) {
-      var element = document.createElement("a");
-      element.setAttribute(
-        "href",
-        "data:text/plain;charset=utf-8," + encodeURIComponent(text)
-      );
-      element.setAttribute("download", filename + ".json");
 
-      element.style.display = "none";
-      document.body.appendChild(element);
+    saveToFile: function (data) {
+      var element = document.createElement('a');
+      element.setAttribute(
+        'href',
+        'data:text/plain;charset=utf-8,' +
+          encodeURIComponent(JSON.stringify(data))
+      );
+      element.setAttribute('download', 'CoNDeT.json');
+
+      element.style.display = 'none';
+      document.body.appendChild(element); //Required on firefox
 
       element.click();
 
       element.remove();
-    }
+    },
   },
 };
