0d54c5a048e6c818f27ed6452de3c30500e0ab49
Hub <hubmiz20@gmail.com>
2021-05-11 16:30:03 +0200

    fix: remove unnecesary variable

--- library/src/script.py
+++ library/src/script.py
@@ -10,12 +10,10 @@ f = open("wholeCode.js","w")
 f.close()
 
 for path,dirs,files in os.walk(start_path):
-    counter = 0
     wholeCode = open("wholeCode.js", 'a')
     for filename in files:
         file = os.path.join(path,filename)
         if ( (re.search(".*\.js",file) != None) ):
-            counter = 1
             f = open(file)
             content = f.readlines()
             for line in content:
