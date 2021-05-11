bead5ae45248f7e289188563e1fbdd983b8138e1
Hub <hubmiz20@gmail.com>
2021-05-11 22:04:50 +0200

    fix: rename file change directory add parameters

--- library/builder.py
+++ library/builder.py
@@ -1,16 +1,20 @@
 import os
 import re
 from pathlib import Path
+import sys
 Path("/wholeCode.js").touch()
 start_path = "." # current directory
 
-files_content =[]
+if ( len(sys.argv) == 2):
+    nazwa = "./dist/" + sys.argv[1] + ".js"
+else:
+    nazwa = "./dist/CoNDeT.js"
 
-f = open("wholeCode.js","w")
+f = open(nazwa,"w")
 f.close()
 
 for path,dirs,files in os.walk(start_path):
-    wholeCode = open("wholeCode.js", 'a')
+    wholeCode = open(nazwa, 'a')
     for filename in files:
         file = os.path.join(path,filename)
         if ( (re.search(".*\.js",file) != None) ):
